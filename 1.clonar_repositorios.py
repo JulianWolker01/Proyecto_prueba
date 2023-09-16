0d87dec188f249f03a627fbb4d460614a6929599
HidalgoFernando <fh017049@gmail.com>
2023-09-16 02:23:47 -0300

    pe

--- 1.clonar_repositorios.py
+++ 1.clonar_repositorios.py
@@ -8,7 +8,7 @@ repos = [
     "https://github.com/JulianWolker01/base-de-datos.git"
 ]
 
-github_token = "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"
+github_token = "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"
 
 for repo_url in repos:
     # Extraer el nombre del repositorio de la URL
