94a113544e962d93bab53b91b0823414c6c2847e
HidalgoFernando <fh017049@gmail.com>
2023-09-18 01:46:40 -0300

    pe

--- todo.py
+++ todo.py
@@ -4,7 +4,7 @@ scripts = [
     "1.clonar_repositorios.py",
     "2.InstalarNODE.py",
     "3.dependencias.py",
-    "4.SelectServer2.py",
+    "4.SelectServer.py",
     "5.BasedeDatos.py",
     "6.configapp.py",
     "7.BackendYFrontend.py"
